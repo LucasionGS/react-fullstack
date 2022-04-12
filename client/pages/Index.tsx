bf9b6e3d97915a20c3ad82ba832ed095f9511f68
Lucasion <lucasion@hotmail.com>
2022-04-12 03:09:15 +0200

    Change title to Home!

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 export default function Index() {
+  document.title = "Home";
   return (
     <div>
-      <h1>Index file</h1>
+      <h1>Home!</h1>
       <p>Welcome to the React app!</p>
     </div>
   );
