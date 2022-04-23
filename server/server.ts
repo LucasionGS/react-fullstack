d64c92c1a5dd995777a1677efd9f0dcc1503db11
Lucasion <lucasion@hotmail.com>
2022-04-23 23:09:39 +0200

    Changed how logging works

--- server/server.ts
+++ server/server.ts
@@ -3,42 +3,36 @@ import Path from "path";
 import { apiRouter } from "./apiRouter";
 import fs from "fs";
 
-const accessLogRecord: string[] = [];
-const errorLogRecord: string[] = [];
-
 const accessLogFile = Path.resolve(__dirname, "../logs/access.log");
 const errorLogFile = Path.resolve(__dirname, "../logs/error.log");
-if (!fs.existsSync(Path.dirname(accessLogFile))) {
-  fs.mkdirSync(Path.dirname(accessLogFile), { recursive: true });
-}
-if (!fs.existsSync(Path.dirname(errorLogFile))) {
-  fs.mkdirSync(Path.dirname(errorLogFile), { recursive: true });
-}
 
-setInterval(() => {
-  if (accessLogRecord.length > 0) {
-    let logFileContent = accessLogRecord.join("\n");
-    fs.promises.appendFile(accessLogFile, logFileContent + "\n");
-    accessLogRecord.length = 0;
+function createLogger<T extends ((...args: any[]) => void) = (message: string) => void>(logFile: string, parser?: T): T {
+  const logRecord: string[] = [];
+  if (!fs.existsSync(Path.dirname(logFile))) {
+    fs.mkdirSync(Path.dirname(logFile), { recursive: true });
   }
-  if (errorLogRecord.length > 0) {
-    let logFileContent = errorLogRecord.join("\n");
-    fs.promises.appendFile(errorLogFile, logFileContent + "\n");
-    errorLogRecord.length = 0;
-  }
-}, 2000);
 
-function accessLog(message: string) {
-  const msg = `[${new Date().toISOString()}] ${message}`;
-  accessLogRecord.push(msg);
-  console.log(msg);
+  return ((...args) => {
+    const msg = parser ? parser(...arguments) : `[${new Date().toISOString()}] ${args[0]}`;
+    if (!parser) {
+      console.log(msg);
+    }
+    fs.promises.appendFile(logFile, msg + "\n");
+
+    setTimeout(() => {
+      if (logRecord.length > 0) {
+        let logFileContent = logRecord.join("\n");
+        fs.promises.appendFile(accessLogFile, logFileContent + "\n");
+        logRecord.length = 0;
+      }
+    }, 2000);
+  }) as T
 }
 
-function errorLog(message: string) {
-  const msg = `[${new Date().toISOString()}] ${message}`;
-  errorLogRecord.push(msg);
-  console.error(msg);
-}
+const accessLog = createLogger(accessLogFile);
+const errorLog = createLogger(errorLogFile,
+  (message: string, errorType?: string) => `[${new Date().toISOString()}]${errorType ? `[${errorType}]`: ""} ${message}`
+);
 
 const app = express();
 const options = {
@@ -47,10 +41,12 @@ const options = {
 
 
 app.listen(options.port, () => {
-  console.log(`Hosted on ${options.port} -> ${__dirname}/../web`);
+  console.log(`Server started on ${options.port} -> ${__dirname}/../web`);
 });
 
+// Logging of page requests
 app.use("/", (req, res, next) => {
+  // Exclude favicon and client.js
   if (req.url !== "/favicon.ico" && req.url !== "/client.js") {
     accessLog(`${req.method} ${req.url} [${req.ip}]`);
   }
@@ -59,7 +55,7 @@ app.use("/", (req, res, next) => {
 
 // Serve API - Put routes in apiRouter in ./api.ts
 app.use("/api", apiRouter, (req, res) => {
-  errorLog(`${req.method} ${req.url} [${req.ip}]`);
+  errorLog(`${req.method} ${req.url} [${req.ip}]`, "MissingEndpoint");
   res.status(404).json({
     error: "Endpoint not found",
   });
